c71ab36844be47c67a73c6078cbdb73ecfb00820
Chris Tieman <iam@christieman.com>
2015-03-18 18:56:28 -0400

    Various style fixes, Some js fixes and commenting
    - Scrollbars are now styled
    - Fixed styling issues with uploading music progress bar
    - Fixed 'Recent Activity' sidebar item

--- js/play-midnight.js
+++ js/play-midnight.js
@@ -1,6 +1,9 @@
 (function($){
 
+// Play Midnight Options
+// Load and Populate options from Chrome storage
 var PlayMidnightOptions = {
+	// Initial Options
 	defaults: {
 		favicon: true,
 		styled: true,
@@ -8,13 +11,20 @@ var PlayMidnightOptions = {
 		theme: 'default'
 	},
 
+	// Load Options from Chrome Storage
+	// Proceed with Callback Function
 	load: function( callback ) {
 		var self = this;
+
+		// Sync options from storage, use defaults as Initial Values
 		chrome.storage.sync.get( self.defaults, function( options ) {
-			callback( options );
+			if (callback && typeof callback === 'function') {
+				callback( options );
+			}
 		});
 	},
 
+	// Populate Options Page (Checkboxes)
 	populate: function( options ) {
 		var self = this;
 
@@ -43,6 +53,7 @@ var PlayMidnightOptions = {
 		});
 	},
 
+	// Update Classes on Checkbox Select
 	doSelect: function( ele ) {
 		var option = $(ele);
 		var group = option.closest('.options-group');
@@ -51,23 +62,29 @@ var PlayMidnightOptions = {
 		option.addClass('selected');
 	},
 
+	// Save Settings to Chrome Storage
 	save: function( callback ) {
+		// Get All Settings Values
 		var favicon = $('#play-midnight-options #favicon').is(':checked');
 		var styled = $('#play-midnight-options #styled').is(':checked');
 		var recentActivity = $('#play-midnight-options #recentActivity').is(':checked');
 		var theme = $('#play-midnight-options .theme-color:checked').attr('id');
 		var status = $('#play-midnight-options #status');
 
+		// Saving to Chrome Storage
 		chrome.storage.sync.set( {
 			favicon: favicon,
 			styled: styled,
 			recentActivity: recentActivity,
 			theme: theme
 		}, function( ) {
+			// Show Status, then call Callback function
 			status.fadeIn(500, function() {
 				setTimeout(function() {
 					status.fadeOut(500, function() {
-						callback();
+						if (callback && typeof callback === 'function') {
+							callback();
+						}
 					});
 				}, 800);
 			});
@@ -75,45 +92,64 @@ var PlayMidnightOptions = {
 	}
 };
 
+
+// Play Midnight Object
+// Load Options, Inject Stylesheet, Favicon, Options, Credits
 var PlayMidnight = {
 
-	options: {
-
-	},
+	options: {},
 
 	init: function() {
 		var self = this;
 
+		// Load Play Midnight Options
 		PlayMidnightOptions.load(function( options ) {
 			self.options = options;
+
+			// Inject Stylesheet
 			self.injectStyle();
-			self.updateFavicon();
-			self.injectOptions( function() {
-				PlayMidnightOptions.populate( self.options );
-				$('#play-midnight-options #save').on( 'click', function(e) {
-					e.preventDefault();
 
-					PlayMidnightOptions.save( function() {
-						location.reload(true);
-					} );
+			// Wait for DOM Before Appending/Updating
+			$(window).load(function() {
+				// Apply New Favicon
+				self.updateFavicon();
+
+				// Inject Options Template
+				self.injectOptions(function() {
+					// Populate Options Template Values
+					PlayMidnightOptions.populate( self.options );
+
+					// Save Options, Refresh Page
+					$('#play-midnight-options #save').on( 'click', function(e) {
+						e.preventDefault();
+
+						PlayMidnightOptions.save( function() {
+							location.reload(true);
+						} );
+					});
+
+					// Hide Options
+					$('#play-midnight-options #cancel').on( 'click', function(e) {
+						e.preventDefault();
+
+						$('#play-midnight-options').removeClass('show');
+					});
 				});
 
-				$('#play-midnight-options #cancel').on( 'click', function(e) {
-					e.preventDefault();
-
-					$('#play-midnight-options').removeClass('show');
-				});
-			});
-			if ( self.options.recentActivity === true ) {
+				// Add Recent Activity/Sorting
 				self.addSortOptions();
-			}
-		});
 
-		this.addCredits();
+				// Add Personal Credits
+				self.addCredits();
+			});
+		});
 	},
 
+	// Inject Stylesheet
 	injectStyle: function() {
 		var style = null;
+
+		// Load Themed Stylesheet
 		if ( this.options.styled === true ) {
 			var theme = this.options.theme;
 			style = $('<link>', {
@@ -122,6 +158,8 @@ var PlayMidnight = {
 				href: chrome.extension.getURL( 'css/play-midnight-' + theme + '.css')
 			});
 			$('head').append(style);
+
+		// Load Minimal Stylesheet for Options Page and Play Midnight Button
 		} else {
 			style = $('<link>', {
 				rel: 'stylesheet',
@@ -132,14 +170,22 @@ var PlayMidnight = {
 		}
 	},
 
+	// Inject Options Template
 	injectOptions: function( callback ) {
+		// Create Options Div
 		var options = $('<div />', {
 			id: 'play-midnight-options',
 		});
-		$.get(chrome.extension.getURL( 'assets/options.html'), function( htmls ) {
+
+		// Load Options Template from Extension
+		$.get(chrome.extension.getURL( 'assets/options.html'), function(htmls) {
+			// Set Options Div HTML
 			options.html( htmls );
+
+			// Append to Body
 			$('body').append( options );
 
+			// Create Play Midnight Button w/ Logo
 			var button = $('<button />', {
 				id: 'btn-pm-options',
 				class: 'button small vertical-align'
@@ -147,31 +193,41 @@ var PlayMidnight = {
 				src: chrome.extension.getURL('icon48.png')
 			})).append( '<span>Play Midnight Options</span>' );
 
+			// Show Options on Click
 			button.on( 'click', function() {
 				$('#play-midnight-options').addClass('show');
 			});
 
+			// Append Button to Navbar
 			$('#headerBar .nav-bar').prepend( button );
 
-			callback();
+			// Callback Function
+			if (callback && typeof callback === 'function') {
+				callback();
+			}
 		});
 	},
 
+	// Update Favicon to Play Midnight version
 	updateFavicon: function() {
-		if ( this.options.favicon === true ) {
-			// Window Load to Prevent Old Favicon from Overwriting
-			$(window).load(function() {
-				var iconUrl = chrome.extension.getURL('images/favicon.ico') + '?v=' + Date.now();
-
-				$('link[rel="SHORTCUT ICON"], link[href="favicon.ico"]').remove();
-				$('head').append( $('<link>', {
-					rel: 'shortcut icon',
-					href: iconUrl
-				}) );
-			});
+		if ( this.options.favicon !== true ) {
+			return;
 		}
+
+		// Load Newest Icon with Timestamp to prevent Caching
+		var iconUrl = chrome.extension.getURL('images/favicon.ico') + '?v=' + Date.now();
+
+		// Remove Old Favicon
+		$('link[rel="SHORTCUT ICON"], link[href="favicon.ico"]').remove();
+
+		// Add New Favicon
+		$('head').append( $('<link>', {
+			rel: 'shortcut icon',
+			href: iconUrl
+		}) );
 	},
 
+	// Add Personal Credits
 	addCredits: function() {
 		var donateUrl = 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KHH9ZJH42FF4J';
 		var personalUrl = 'http://christieman.com/';
@@ -201,7 +257,12 @@ var PlayMidnight = {
 		}
 	},
 
+	// Add Recent Activity and Sorting options
 	addSortOptions: function() {
+		if ( this.options.recentActivity !== true ) {
+			return;
+		}
+
 		var sortHtml = [
 			'<div id="recent-sort" class="tab-container">',
 			' <a class="header-tab-title selected" data-reason="0">All</a>',
@@ -212,14 +273,13 @@ var PlayMidnight = {
 		].join('');
 
 		// Add a link directly to Recent, after the first "Listen Now" link
-		$('<a data-type="recent" class="nav-item-container tooltip" href="">Recent Activity</a>')
-			.insertAfter('#nav_collections a:nth-child(2)');
+		$('<a data-type="recent" class="nav-item-container tooltip" href="">Recent Activity</a>').insertAfter('#nav_collections a:nth-child(2)');
 
 		// Add sort links to the header
 		function toggleRecentUI() {
 			// Only add them if "Recent" string is present and we're not already in this view.
 			// Otherwise remove the UI completely.
-			if ( $(this).children('.tab-text:contains(Recent)').length && ! $(this).children('#recent-sort').length ) {
+			if ( $(this).children('.tab-text:contains(Recent)').length && !$(this).children('#recent-sort').length ) {
 				$(this).append(sortHtml);
 			} else {
 				$('#recent-sort').remove();
@@ -245,6 +305,8 @@ var PlayMidnight = {
 	}
 };
 
+
+// Load Play Midnight
 PlayMidnight.init();
 
 })(jQuery);
