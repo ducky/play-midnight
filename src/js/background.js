c86c63767f2e9581e364d00b314388ec7940651c
Chris Tieman <iam@christieman.com>
2015-10-27 21:26:25 -0400

    Added new background script to deal with generating favicons

---
+++ src/js/background.js
@@ -0,0 +1,38 @@
+/*global chrome */
+(function() {
+	'use strict';
+
+	function getIcon(request, sender, cb) {
+		var context;
+		var img = document.createElement('img');
+		var canvas = document.createElement("canvas");
+		var width = 48;
+		var height = 48;
+
+		canvas.width = width;
+		canvas.height = height;
+		canvas.setAttribute('crossOrigin', 'anonymous');
+		context = canvas.getContext("2d");
+
+		img.addEventListener('load', createIcon);
+		img.src = request.url;
+
+		function createIcon() {
+			var icon;
+
+			// Create Colored Icon
+			context.drawImage(img, 0, 0);
+			context.globalCompositeOperation = "source-in";
+			context.fillStyle = request.color;
+			context.fillRect(0, 0, width, height);
+			context.fill();
+
+			icon = canvas.toDataURL();
+
+			console.log(icon);
+
+			cb(icon);
+		}
+	}
+	chrome.extension.onRequest.addListener(getIcon);
+}());
