51c7f4144e6e86ecd1085b4a12ce4212ee24dc0b
Chris Tieman <iam@christieman.com>
2016-09-03 03:51:38 -0400

    Updated to v2.3.7

--- src/js/play-midnight-core.js
+++ src/js/play-midnight-core.js
@@ -5,7 +5,7 @@ var PlayMidnight = (function (_) {
   // Dev Mode: Use CSS File rather than inline <style> (inline allows dynamic accent colors)
   var _dev = false;
 
-  var VERSION_NUMBER = '2.3.6';
+  var VERSION_NUMBER = '2.3.7';
 
   // Reset Options when version less than
   var _resetOptions = '2.1.0';
