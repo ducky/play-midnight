49e0df99130acadf91a0a550e8809b197324c051
Chris Tieman <iam@christieman.com>
2018-01-03 09:20:12 -0500

    Updated options with new hoc

--- src/options/components/1 - General/Enabled.js
+++ src/options/components/1 - General/Enabled.js
@@ -1,19 +1,34 @@
 import { PureComponent } from 'react';
+import { connect } from 'react-redux';
 import { css } from 'styled-components';
+
+import { selectors } from 'modules/options';
 
 import withStyles from 'hoc/withStyles';
 
 const OPTION_ID = 'enabled';
 
-const styles = css`
-  body {
-    color: #f00;
-  }
-`;
+const mapStateToProps = state => ({
+  accentColor: selectors.accentColor(state)
+});
 
-@withStyles(OPTION_ID, styles)
+@withStyles(OPTION_ID)
+@connect(mapStateToProps)
 class Enabled extends PureComponent {
+  componentWillUnmount() {
+    this.props.removeStyles();
+  }
+
   render() {
+    const styles = css`
+      body {
+        background: #141517;
+        transition: background 300ms;
+      }
+    `;
+
+    this.props.updateStyles(styles);
+
     return null;
   }
 }
