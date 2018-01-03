5295d50365c776888dd8f34bda7eab682c75b53b
Chris Tieman <iam@christieman.com>
2018-01-03 09:21:55 -0500

    Added loadBackground api method, IS_EXTENSION check

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,5 @@
 const LOCAL_STORAGE_KEY = 'PLAY_MIDNIGHT';
+const IS_EXTENSION = chrome.storage !== undefined;
 
 export const getUrl = url => {
   if (chrome.extension) {
@@ -10,7 +11,7 @@ export const getUrl = url => {
 
 export const load = async data => {
   return new Promise(resolve => {
-    if (chrome.storage) {
+    if (IS_EXTENSION) {
       return chrome.storage.sync.get(data, resolve);
     } else {
       const storageContents = localStorage.getItem(LOCAL_STORAGE_KEY);
@@ -22,9 +23,21 @@ export const load = async data => {
   });
 };
 
+export const loadBackground = async data => {
+  return new Promise(resolve => {
+    if (IS_EXTENSION) {
+      console.log(chrome.runtime);
+      return chrome.runtime.sendMessage(data, resolve);
+    } else {
+      console.log('Load Background Fallback!', data);
+      resolve(data);
+    }
+  });
+};
+
 export const save = async data => {
   return new Promise(resolve => {
-    if (chrome.storage) {
+    if (IS_EXTENSION) {
       return chrome.storage.sync.set(data, resolve);
     } else {
       localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data));
