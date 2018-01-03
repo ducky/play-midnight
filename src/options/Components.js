9dfcbf39e1a0d33d01a20c14fd2a397fb24d36ff
Chris Tieman <iam@christieman.com>
2018-01-03 09:22:20 -0500

    Updated layout of options Components file

--- src/options/Components.js
+++ src/options/Components.js
@@ -1,15 +1,25 @@
 import Core from './components/0 - Core/Core';
 import FAB from './components/0 - Core/FAB';
-
 import AccentsOnly from './components/1 - General/AccentsOnly';
 import Enabled from './components/1 - General/Enabled';
-
 import Favicon from './components/3 - Customize/Favicon';
 
-export default {
-  [AccentsOnly.id]: AccentsOnly,
+const CORE = {
   [Core.id]: Core,
-  [Enabled.id]: Enabled,
-  [FAB.id]: FAB,
+  [FAB.id]: FAB
+};
+
+const GENERAL = {
+  [AccentsOnly.id]: AccentsOnly,
+  [Enabled.id]: Enabled
+};
+
+const CUSTOMIZE = {
   [Favicon.id]: Favicon
+};
+
+export default {
+  ...CORE,
+  ...GENERAL,
+  ...CUSTOMIZE
 };
