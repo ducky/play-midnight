3c3651a3e23b8b5d143d12da7a612b645ebda0df
Chris Tieman <iam@christieman.com>
2018-01-08 01:46:43 -0500

    Updated getInjectedElement to allow for Mutation-based inserting if an element is still loading

--- src/utils/getInjectedElement.js
+++ src/utils/getInjectedElement.js
@@ -1,18 +1,52 @@
-export default (type, attributes = { id: '' }, where = 'body') => {
-  let element = document.querySelector(`${type}#${attributes.id}`);
+const createElement = (type, attrs) => {
+  const element = document.createElement(type);
 
-  if (element) {
-    return element;
-  }
-
-  const container = document.querySelector(where);
-  element = document.createElement(type);
-
-  Object.keys(attributes).forEach(key => {
-    const attribute = attributes[key];
+  Object.keys(attrs).forEach(key => {
+    const attribute = attrs[key];
     element[key] = attribute;
   });
 
-  container.appendChild(element);
   return element;
+};
+
+const injectElement = (element, where, prepend = false) => {
+  const container = document.querySelector(where);
+
+  if (container) {
+    if (prepend) {
+      container.insertAdjacentElement('afterbegin', element);
+    } else {
+      container.appendChild(element);
+    }
+
+    return element;
+  }
+
+  return undefined;
+};
+
+export default async (type, attributes, where = 'body') => {
+  const { prepend, ...attrs } = attributes;
+
+  return new Promise(resolve => {
+    // Already Exists
+    const element = document.querySelector(`${type}#${attrs.id}`);
+    if (element) return resolve(element);
+
+    // New Element
+    const create = createElement(type, attrs);
+    const injected = injectElement(create, where, prepend);
+    if (injected) return resolve(create);
+
+    // Inject Target Not Found
+    const observer = new MutationObserver(mutations => {
+      const injected = injectElement(create, where, prepend);
+      if (injected) {
+        observer.disconnect();
+        return resolve(create);
+      }
+    });
+
+    observer.observe(document.body, { childList: true });
+  });
 };
