2c280ff617b5035724dc2b15de729166f17422f1
Chris Tieman <iam@christieman.com>
2018-01-08 02:27:00 -0500

    Updated id validation (?) to use uuid to prevent collision on duplicate names

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,10 +1,14 @@
+import uuid from 'uuid/v1';
+
 export const validateTitle = (title = '') => {
   return title.replace(/[^a-zA-Z0-9',"()\s]/g, '');
 };
 
 export const validateId = (id = '') => {
-  return id
+  const cleansed = id
     .replace(/[^a-zA-Z0-9\s]/g, '')
     .replace(/\s/g, '-')
     .toLowerCase();
+
+  return `${cleansed}-${uuid()}`;
 };
