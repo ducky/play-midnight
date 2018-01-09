8aa498a7e539e6225b586bb6d5cbd572cfa9e59e
Chris Tieman <iam@christieman.com>
2018-01-08 21:09:59 -0500

    Added super basic test logic for Modals/Toasts

--- src/components/PlayMidnight.js
+++ src/components/PlayMidnight.js
@@ -3,8 +3,11 @@ import { connect } from 'react-redux';
 import styled from 'styled-components';
 
 import { actions, selectors } from 'modules/options';
+import { actions as modalActions } from 'modules/modal';
+import { actions as toastActions } from 'modules/toast';
 import Components from 'options/Components';
 
+import Button from './Button';
 import PlayMidnightOptions from './PlayMidnightOptions';
 
 const Wrapper = styled.div`
@@ -20,7 +23,9 @@ const mapStateToProps = state => ({
 });
 
 @connect(mapStateToProps, {
-  fetchOptions: actions.fetchOptions
+  fetchOptions: actions.fetchOptions,
+  showModal: modalActions.showModal,
+  createToast: toastActions.createToast
 })
 class PlayMidnight extends PureComponent {
   componentDidMount() {
@@ -28,7 +33,7 @@ class PlayMidnight extends PureComponent {
   }
 
   render() {
-    const { options } = this.props;
+    const { options, createToast, showModal } = this.props;
 
     const renderOptions = options => {
       return options
@@ -47,6 +52,22 @@ class PlayMidnight extends PureComponent {
 
     return (
       <Wrapper>
+        <Button
+          onClick={() => {
+            showModal('alert', {
+              title: 'Nice Buns',
+              message: 'You has nice buns',
+              closeText: 'Thanks! 😭',
+              onClose: () =>
+                createToast('success', {
+                  title: 'Buns Success!',
+                  message: 'You rocked it!'
+                })
+            });
+          }}
+        >
+          Modal
+        </Button>
         <PlayMidnightOptions />
         {renderOptions(options)}
       </Wrapper>
