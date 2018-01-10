70b8a138567f241c6ee5da8ed119e5e9b4d9fc0a
Chris Tieman <iam@christieman.com>
2018-01-10 03:26:29 -0500

    Added a ColorPickerModal

---
+++ src/components/Modals/ColorPickerModal.js
@@ -0,0 +1,97 @@
+import React, { PureComponent } from 'react';
+import { PhotoshopPicker } from 'react-color';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+
+import { actions } from 'modules/modal';
+
+import ModalWrapper from './ModalWrapper';
+
+const FancyInput = styled.input`
+  width: 100%;
+  text-align: center;
+  background: transparent;
+  outline: none;
+  box-shadow: none;
+  color: #ececec;
+  text-shadow: 0px 3px #141517;
+  border: none;
+  font-size: 36px;
+  font-weight: 700;
+  margin: 0 0 25px;
+`;
+
+const PickerWrapper = styled.div`
+  box-shadow: 0 11px 7px 0 rgba(0, 0, 0, 0.19), 0 13px 25px 0 rgba(0, 0, 0, 0.3);
+  color: #000;
+  margin: 0 0 10px;
+`;
+
+@connect(null, { close: actions.closeModal })
+class ColorPickerModal extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    const details = props.details || {};
+    this.state = {
+      id: undefined,
+      color: '',
+      name: '',
+      ...details,
+    };
+  }
+
+  onTitleChange = ({ target }) => {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const id = target.name;
+    this.setState(state => ({ [id]: value }));
+  };
+
+  onColorChange = color => {
+    this.setState(state => ({
+      color: color.hex,
+    }));
+  };
+
+  closeModal = acceptValue => {
+    const { id, color, name } = this.state;
+    const { id: modalId, onClose, close } = this.props;
+
+    if (acceptValue) {
+      onClose({ id, color, name });
+    }
+
+    close(modalId);
+  };
+
+  render() {
+    const { color, name } = this.state;
+
+    return (
+      <ModalWrapper {...this.props} closeButton={false} cancelButton={false} width={513}>
+        <FancyInput
+          name="name"
+          placeholder="Click Here to Name"
+          onChange={this.onTitleChange}
+          value={name}
+          type="text"
+        />
+        <PickerWrapper>
+          <PhotoshopPicker
+            color={color}
+            onChange={this.onColorChange}
+            onAccept={() => this.closeModal(true)}
+            onCancel={() => this.closeModal(false)}
+          />
+        </PickerWrapper>
+        <p>
+          <small style={{ display: 'block', textAlign: 'center' }}>
+            Play around until you find the perfect color. You'll thank me later.
+          </small>
+        </p>
+      </ModalWrapper>
+    );
+  }
+}
+
+export default ColorPickerModal;
