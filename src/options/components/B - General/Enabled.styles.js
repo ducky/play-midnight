013dd7778a3ccc1eb3e68e760333e64bbd0f3d75
Chris Tieman <iam@christieman.com>
2018-01-10 11:09:48 -0500

    Updated Enabled option to style player

--- src/options/components/B - General/Enabled.styles.js
+++ src/options/components/B - General/Enabled.styles.js
@@ -1,9 +1,15 @@
 import { css } from 'styled-components';
 
+import { colors } from 'style/theme';
+
 const getStyles = accentColor => css`
   body {
     background: #141517;
     transition: background 300ms;
+  }
+
+  #player {
+    background: ${colors.background_player};
   }
 `;
 
