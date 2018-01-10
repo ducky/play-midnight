588c0293a88b0c8373bda205b202727370de0600
Chris Tieman <iam@christieman.com>
2018-01-10 11:08:26 -0500

    Removed modal test button from PlayMidnight, separated styles

--- src/components/PlayMidnight.js
+++ src/components/PlayMidnight.js
@@ -1,27 +1,14 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
 
 import { actions } from 'modules/options';
-import { actions as modalActions } from 'modules/modal';
-import { actions as toastActions } from 'modules/toast';
 import Components from 'options/Components';
 
-import Button from './Button';
 import PlayMidnightOptions from './PlayMidnightOptions';
-
-const Wrapper = styled.div`
-  font-family: 'Roboto', Helvetica, sans-serif;
-
-  * {
-    box-sizing: border-box;
-  }
-`;
+import StyledPlayMidnight from './PlayMidnight.styled';
 
 @connect(null, {
   fetchOptions: actions.fetchOptions,
-  showModal: modalActions.showModal,
-  createToast: toastActions.createToast,
 })
 class PlayMidnight extends PureComponent {
   componentDidMount() {
@@ -29,33 +16,15 @@ class PlayMidnight extends PureComponent {
   }
 
   render() {
-    const { createToast, showModal } = this.props;
-
     const renderOptions = options => {
       return Components.map((Component, i) => <Component key={i} />);
     };
 
     return (
-      <Wrapper>
-        <Button
-          onClick={() => {
-            showModal('alert', {
-              title: 'Nice Buns',
-              message: 'You has nice buns',
-              closeText: 'Thanks! 😭',
-              onClose: () =>
-                createToast('success', {
-                  title: 'Buns Success!',
-                  message: 'You rocked it!',
-                }),
-            });
-          }}
-        >
-          Modal
-        </Button>
+      <StyledPlayMidnight>
         <PlayMidnightOptions />
         {renderOptions()}
-      </Wrapper>
+      </StyledPlayMidnight>
     );
   }
 }
---
+++ src/components/PlayMidnight.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const PlayMidnight = styled.div`
+  font-family: 'Roboto', Helvetica, sans-serif;
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export default PlayMidnight;
