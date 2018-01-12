dac60d8c82964d0b37df3a01328a1832c20c2ec9
Chris Tieman <iam@christieman.com>
2018-01-12 02:48:13 -0500

    Updated getCssString to use stylis - copying styled-components internal logic

--- src/utils/getCssString.js
+++ src/utils/getCssString.js
@@ -1,7 +1,6 @@
+import stylis from 'stylis';
+
 export default (css = []) => {
-  const cssString = css
-    .join('')
-    .replace(/[\n\r\t]/gi, '')
-    .replace(/[\s]{2,}/g, ' ');
-  return cssString;
+  const flatCSS = css.join('').replace(/^\s*\/\/.*$/gm, ''); // replace JS comments
+  return stylis('', flatCSS);
 };
