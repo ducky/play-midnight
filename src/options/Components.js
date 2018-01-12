0e4c3eeb6d781d8cf9cddbbd5f130d611d420216
Chris Tieman <iam@christieman.com>
2018-01-12 02:52:42 -0500

    Created Queue component

--- src/options/Components.js
+++ src/options/Components.js
@@ -3,13 +3,14 @@ import FAB from './components/A - Core/FAB';
 import AccentsOnly from './components/B - General/AccentsOnly';
 import Enabled from './components/B - General/Enabled';
 import Favicon from './components/C - Customize/Favicon';
+import Queue from './components/C - Customize/Queue';
 import Menus from './components/D - Menus/Menus';
 import Playlists from './components/E - Playlists/Playlists';
 import SoundSearch from './components/E - Playlists/SoundSearch';
 
 const CORE = [Core, FAB];
 const GENERAL = [AccentsOnly, Enabled];
-const CUSTOMIZE = [Favicon];
+const CUSTOMIZE = [Favicon, Queue];
 const MENUS = [Menus];
 const PLAYLISTS = [Playlists, SoundSearch];
 
