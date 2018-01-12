23074e725631ed0b34d37d9d43e23152c507f2a1
Chris Tieman <iam@christieman.com>
2018-01-12 02:54:43 -0500

    Updated Menus to use withStyles

--- src/options/components/D - Menus/Menus.styles.js
+++ src/options/components/D - Menus/Menus.styles.js
@@ -1,39 +1,47 @@
 import { css } from 'styled-components';
 
-const getStyles = () => ({
+import createStylesheet from 'utils/createStylesheet';
+
+const styles = () => ({
   myLibrary: css`
     #nav_collections a[data-type='mylibrary'],
-    #quickNavContainer gpm-quick-nav-item[view-type='mylibrary'] {
+    #quickNavContainer gpm-quick-nav-item[view-type='mylibrary'],
+    #quick-nav-container gpm-quick-nav-item[view-type='mylibrary'] {
       display: none;
     }
   `,
   recent: css`
     #nav_collections a[data-type='recents'],
-    #quickNavContainer gpm-quick-nav-item[view-type='recents'] {
+    #quickNavContainer gpm-quick-nav-item[view-type='recents'],
+    #quick-nav-container gpm-quick-nav-item[view-type='recents'] {
       display: none;
     }
   `,
   topCharts: css`
     #nav_collections a[data-type='wtc'],
-    #quickNavContainer gpm-quick-nav-item[view-type='wtc'] {
+    #quickNavContainer gpm-quick-nav-item[view-type='wtc'],
+    #quick-nav-container gpm-quick-nav-item[view-type='wtc'] {
       display: none;
     }
   `,
   newReleases: css`
     #nav_collections a[data-type='wnr'],
-    #quickNavContainer gpm-quick-nav-item[view-type='wnr'] {
+    #quickNavContainer gpm-quick-nav-item[view-type='wnr'],
+    #quick-nav-container gpm-quick-nav-item[view-type='wnr'] {
       display: none;
     }
   `,
   browseStations: css`
     #nav_collections a[data-type='wbs'],
-    #quickNavContainer gpm-quick-nav-item[view-type='wbs'] {
+    #quickNavContainer gpm-quick-nav-item[view-type='wbs'],
+    #quick-nav-container gpm-quick-nav-item[view-type='wbs'] {
       display: none;
     }
   `,
   podcasts: css`
     #nav_collections a[data-type='tps'],
-    #quickNavContainer gpm-quick-nav-item[view-type='tps'] {
+    #quickNavContainer gpm-quick-nav-item[view-type='tps'],
+    #quick-nav-container gpm-quick-nav-item[view-type='tps'] {
       display: none;
     }
   `,
@@ -49,4 +57,4 @@ const getStyles = () => ({
   `,
 });
 
-export default getStyles;
+export default createStylesheet(styles);
