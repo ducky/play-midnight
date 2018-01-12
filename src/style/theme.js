2ce73fa74a8a8695fb9fd4230319a89892b33aef
Chris Tieman <iam@christieman.com>
2018-01-11 23:14:14 -0500

    Documented styles/theme a little bit

--- src/style/theme.js
+++ src/style/theme.js
@@ -21,17 +21,27 @@ export const colors = {
   font_secondary: '#888',
   blue: '#007AA5',
   green: '#009d00',
-  red: '#9d0000'
+  red: '#9d0000',
 };
 
+// Nav
 colors.background_nav = lighten(colors.background_dark, 3);
-colors.background_page = lighten(colors.background_dark, 3);
-colors.background_card = lighten(colors.background_dark, 3);
-colors.background_player = lighten(colors.background_dark, 5);
-colors.background_menu = lighten(colors.background_dark, 5);
-colors.background_search = lighten(colors.background_dark, 8);
-
 colors.border_nav = darken(colors.background_nav, 5);
+
+// Page
+colors.background_page = lighten(colors.background_dark, 3);
 colors.border_page = darken(colors.background_page, 5);
+
+// Cards
+colors.background_card = lighten(colors.background_dark, 3);
+
+// Player
+colors.background_player = lighten(colors.background_dark, 5);
+
+// Menu
+colors.background_menu = lighten(colors.background_dark, 5);
 colors.border_menu = darken(colors.background_menu, 5);
+
+// Search
+colors.background_search = lighten(colors.background_dark, 8);
 colors.border_search = darken(colors.background_search, 3);
