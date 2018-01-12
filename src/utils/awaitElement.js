ee23b7492ee72a19c947586699446a4e320d0e63
Chris Tieman <iam@christieman.com>
2018-01-11 23:11:52 -0500

    Created awaitElement util that waits for element to exist before resolving promise

---
+++ src/utils/awaitElement.js
@@ -0,0 +1,20 @@
+const awaitElement = async (where = 'body') => {
+  return new Promise(resolve => {
+    // Already Exists
+    const target = document.querySelector(where);
+    if (target) return resolve(target);
+
+    // Target Not Found
+    const observer = new MutationObserver(mutations => {
+      const target = document.querySelector(where);
+      if (target) {
+        observer.disconnect();
+        return resolve(target);
+      }
+    });
+
+    observer.observe(document.body, { childList: true });
+  });
+};
+
+export default awaitElement;
