fc9e199c05ae6a04ec236a27598d53c921484c20
Chris Tieman <iam@christieman.com>
2018-01-24 01:23:16 -0500

    Added some generic notification files to start fleshing things out for notifications

---
+++ src/notifications/index.js
@@ -0,0 +1,7 @@
+import defaultNotification from './templates/default';
+import v3_0_0 from './templates/3.0.0';
+
+export default {
+  default: defaultNotification,
+  '3.0.0': v3_0_0,
+};
---
+++ src/notifications/templates/default.js
@@ -0,0 +1,11 @@
+const Notification = {
+  buttonText: 'Get Started!',
+  content: [
+    {
+      type: 'Text',
+      value: `Thanks for checking out Play Midnight!`,
+    },
+  ],
+};
+
+export default Notification;
