d3ccd84c3e476e0abed48e41f96d20ecaaa725e8
Chris Tieman <iam@christieman.com>
2018-01-24 01:26:17 -0500

    Removed text accent color from Core styles

--- src/options/components/A - Core/Core.styles.js
+++ src/options/components/A - Core/Core.styles.js
@@ -3,11 +3,6 @@ import { css } from 'styled-components';
 import createStylesheet from 'utils/createStylesheet';
 
 const styles = accentColor => css`
-  body {
-    color: ${accentColor};
-    transition: color 300ms;
-  }
-
   /* Recent Activity Images */
   sj-scrolling-module[module-token='CLIENT_SIDE_RECENTS'] sj-card {
     width: 140px !important;
