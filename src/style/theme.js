73700ffc96c984e4f30e27f3d689dd7ef7529570
Chris Tieman <iam@christieman.com>
2018-01-25 18:19:43 -0500

    Updated theme to follow closer to how Material UI works

--- src/style/theme.js
+++ src/style/theme.js
@@ -28,6 +28,8 @@ export const lighten = (c, amount) => {
 
 export const DEFAULT_ACCENT = '#ec4e28';
 export const DEFAULT_BACKGROUND = '#141517';
+export const FONT_LIGHT = '#141517';
+export const FONT_DARK = '#ececec';
 export const TRANSITION_LIGHTNING = '200ms';
 export const TRANSITION_FAST = '300ms';
 export const TRANSITION_MEDIUM = '500ms';
@@ -36,33 +38,32 @@ export const TRANSITION_SLOW = '700ms';
 export const stripTransition = transition => parseInt(transition.replace('ms', ''), 10);
 
 export const createTheme = (background = DEFAULT_BACKGROUND, accent = DEFAULT_ACCENT) => ({
-  accent,
-  background,
-  font_primary: isLight(background) ? '#141517' : '#ececec',
-  font_secondary: isLight(background) ? '#555' : '#888',
-  blue: '#007AA5',
-  green: '#009d00',
+  black: '#000',
+  white: '#fff',
+  font_primary: isLight(background) ? FONT_LIGHT : FONT_DARK,
+  font_secondary: isLight(background) ? lighten(FONT_LIGHT, 25) : darken(FONT_DARK, 25),
   red: '#9d0000',
 
-  // Nav
-  background_nav: lighten(background, 3),
-  border_nav: darken(background, 3),
+  B25: lighten(background, 13),
+  B50: lighten(background, 11),
+  B100: lighten(background, 9),
+  B200: lighten(background, 7),
+  B300: lighten(background, 5),
+  B400: lighten(background, 3),
+  B500: background,
+  B600: darken(background, 3),
+  B700: darken(background, 5),
+  B800: darken(background, 7),
+  B900: darken(background, 9),
 
-  // Page
-  background_page: lighten(background, 3),
-  border_page: darken(background, 3),
-
-  // Cards
-  background_card: lighten(background, 3),
-
-  // Player
-  background_player: lighten(background, 5),
-
-  // Menu
-  background_menu: lighten(background, 5),
-  border_menu: darken(background, 1),
-
-  // Search
-  background_search: lighten(background, 8),
-  border_search: lighten(background, 2),
+  A50: lighten(accent, 11),
+  A100: lighten(accent, 9),
+  A200: lighten(accent, 7),
+  A300: lighten(accent, 5),
+  A400: lighten(accent, 3),
+  A500: accent,
+  A600: darken(accent, 3),
+  A700: darken(accent, 5),
+  A800: darken(accent, 7),
+  A900: darken(accent, 9),
 });
