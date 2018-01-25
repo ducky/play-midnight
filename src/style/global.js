a5690c85d1889624b76f9548e1f24c9fdccc7ef2
Chris Tieman <iam@christieman.com>
2018-01-25 02:51:12 -0500

    Started working towards styling

--- src/style/global.js
+++ src/style/global.js
@@ -1,7 +1,32 @@
 import { injectGlobal } from 'styled-components';
 
+import { getUrl } from 'lib/api';
+import PlayMidnightLogo from 'assets/images/play-logo-dark.png';
+
 injectGlobal`
   body{
     -webkit-font-smoothing: antialiased;
+  }
+
+  #splash-screen {
+  #loading-progress-content {
+    position: relative;
+
+    &:before {
+      position: absolute;
+      top: 0;
+      left: 50%;
+      transform: translateX(-50%);
+      content: '';
+      width: 256px;
+      height: 256px;
+      background: url(${getUrl(PlayMidnightLogo)});
+      background-size: 256px 256px;
+    }
+
+    svg {
+      visibility: hidden;
+      opacity: 0;
+    }
   }
 `;
