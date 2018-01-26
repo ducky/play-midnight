7ceeabd276e6e780088cd2b52fbbfb177ea7d398
Chris Tieman <iam@christieman.com>
2018-01-26 02:27:06 -0500

    Updated PlayMidnightOptions to allow color duplication and used new Icon components

--- src/components/PlayMidnightOptions/Option/OptionArray.js
+++ src/components/PlayMidnightOptions/Option/OptionArray.js
@@ -2,11 +2,15 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 
 import { actions } from 'modules/modal';
-import { replaceItem, removeItem } from 'utils/array';
+import { insertAt, replaceItem, removeItem } from 'utils/array';
 import { validateId, validateTitle } from 'utils/validation';
 
 import Button from 'components/Button';
 import StyledOption, { CollectionItem } from './Option.styled';
+import IconCopy from 'components/Icons/IconCopy';
+import IconDropper from 'components/Icons/IconDropper';
+import IconPencil from 'components/Icons/IconPencil';
+import IconTrash from 'components/Icons/IconTrash';
 
 // TODO - Update to be dynamic and allow for createable - false
 @connect(null, { showModal: actions.showModal })
@@ -66,6 +70,27 @@ class Option extends PureComponent {
     });
   };
 
+  duplicateColor = (e, { name: rawName, color }, index) => {
+    e.preventDefault();
+
+    const { plural, values, onChangeValues, showModal } = this.props;
+
+    const name = validateTitle(rawName);
+    const id = validateId(name);
+
+    console.log(id, name);
+
+    onChangeValues({
+      id: plural,
+      value: insertAt(values, { id, name, value: color }, index),
+    });
+
+    showModal('colorPicker', {
+      details: { id, name, color },
+      onClose: this.saveColor,
+    });
+  };
+
   editColor = (e, details) => {
     e.preventDefault();
     const { showModal } = this.props;
@@ -96,13 +121,14 @@ class Option extends PureComponent {
           </div>
           <div className="Option__action">
             <Button className="Option__action-button" onClick={this.editColor}>
-              NEW<br />COLOR
+              <IconDropper style={{ width: 10, height: 10, marginRight: 5 }} />
+              <div style={{ textAlign: 'center', fontSize: 10 }}>NEW COLOR</div>
             </Button>
           </div>
         </div>
 
         <div className="Option__body Option__collection">
-          {values.map(({ id: colorId, name, value: colorValue }) => (
+          {values.map(({ id: colorId, name, value: colorValue }, i) => (
             <CollectionItem
               className="Option__collection-item"
               key={colorId}
@@ -122,13 +148,29 @@ class Option extends PureComponent {
                 <div className="CollectionItem__field">{colorValue.toUpperCase()}</div>
               </div>
               <div
-                className="CollectionItem__edit"
+                className="CollectionItem__icon CollectionItem__edit"
+                title="Edit Color"
+                aria-label="Edit Color"
                 onClick={e => this.editColor(e, { id: colorId, name, color: colorValue })}
-              />
+              >
+                <IconPencil style={{ width: 14, height: 14 }} />
+              </div>
               <div
-                className="CollectionItem__remove"
+                className="CollectionItem__icon CollectionItem__copy"
+                title="Copy Color"
+                aria-label="Copy Color"
+                onClick={e => this.duplicateColor(e, { name, color: colorValue }, i)}
+              >
+                <IconCopy style={{ width: 14, height: 14 }} />
+              </div>
+              <div
+                className="CollectionItem__icon CollectionItem__remove"
+                title="Remove Color"
+                aria-label="Remove Color"
                 onClick={e => this.removeItem(e, { id: colorId, name, color: colorValue })}
-              />
+              >
+                <IconTrash style={{ width: 14, height: 14 }} />
+              </div>
             </CollectionItem>
           ))}
         </div>
