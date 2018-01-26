81b98f0b8ab98bcf2a63d7f17f1a1a258ee7432f
Chris Tieman <iam@christieman.com>
2018-01-26 02:27:35 -0500

    Updated validation to allow periods

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,7 +1,7 @@
 import uuid from 'uuid/v1';
 
 export const validateTitle = (title = '') => {
-  return title.replace(/[^a-zA-Z0-9',"()\s]/g, '');
+  return title.replace(/[^a-zA-Z0-9.',"()\s]/g, '');
 };
 
 export const validateId = (id = '') => {
