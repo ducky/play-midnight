916f619a3d359f136185a50d892fe13bfdabb93a
Chris Tieman <iam@christieman.com>
2018-01-26 02:33:41 -0500

    Cleaned up OptionArray

--- src/components/PlayMidnightOptions/Option/OptionArray.js
+++ src/components/PlayMidnightOptions/Option/OptionArray.js
@@ -15,13 +15,7 @@ import IconTrash from 'components/Icons/IconTrash';
 // TODO - Update to be dynamic and allow for createable - false
 @connect(null, { showModal: actions.showModal })
 class Option extends PureComponent {
-  state = {
-    color: '',
-    name: '',
-    showForm: false,
-  };
-
-  removeItem = (e, { id, name, color }) => {
+  removeColor = (e, { id, name, color }) => {
     const { id: optionId, defaultValues, plural, value, values, onChange, onChangeValues, showModal } = this.props;
 
     e.preventDefault();
@@ -91,7 +85,9 @@ class Option extends PureComponent {
 
   editColor = (e, details) => {
     e.preventDefault();
+
     const { showModal } = this.props;
+
     showModal('colorPicker', {
       details,
       onClose: this.saveColor,
@@ -110,6 +106,47 @@ class Option extends PureComponent {
   render() {
     const { id, title, description, theme, value, values, onTargetedChange } = this.props;
 
+    const renderItems = items =>
+      items.map(({ id: colorId, name, value: colorValue }, i) => (
+        <CollectionItem
+          className="Option__collection-item"
+          key={colorId}
+          background={colorValue}
+          selected={value === colorId}
+          theme={theme}
+        >
+          <input name={id} value={colorId} defaultChecked={value === colorId} onClick={onTargetedChange} type="radio" />
+          <div className="CollectionItem__fields">
+            <div className="CollectionItem__field CollectionItem__field--title">{name}</div>
+            <div className="CollectionItem__field">{colorValue.toUpperCase()}</div>
+          </div>
+          <div
+            className="CollectionItem__icon CollectionItem__edit"
+            title="Edit Color"
+            aria-label="Edit Color"
+            onClick={e => this.editColor(e, { id: colorId, name, color: colorValue })}
+          >
+            <IconPencil style={{ width: 14, height: 14 }} />
+          </div>
+          <div
+            className="CollectionItem__icon CollectionItem__copy"
+            title="Copy Color"
+            aria-label="Copy Color"
+            onClick={e => this.duplicateColor(e, { name, color: colorValue }, i)}
+          >
+            <IconCopy style={{ width: 14, height: 14 }} />
+          </div>
+          <div
+            className="CollectionItem__icon CollectionItem__remove"
+            title="Remove Color"
+            aria-label="Remove Color"
+            onClick={e => this.removeColor(e, { id: colorId, name, color: colorValue })}
+          >
+            <IconTrash style={{ width: 14, height: 14 }} />
+          </div>
+        </CollectionItem>
+      ));
+
     return (
       <StyledOption>
         <div className="Option__header">
@@ -125,53 +162,7 @@ class Option extends PureComponent {
           </div>
         </div>
 
-        <div className="Option__body Option__collection">
-          {values.map(({ id: colorId, name, value: colorValue }, i) => (
-            <CollectionItem
-              className="Option__collection-item"
-              key={colorId}
-              background={colorValue}
-              selected={value === colorId}
-              theme={theme}
-            >
-              <input
-                name={id}
-                value={colorId}
-                defaultChecked={value === colorId}
-                onClick={onTargetedChange}
-                type="radio"
-              />
-              <div className="CollectionItem__fields">
-                <div className="CollectionItem__field CollectionItem__field--title">{name}</div>
-                <div className="CollectionItem__field">{colorValue.toUpperCase()}</div>
-              </div>
-              <div
-                className="CollectionItem__icon CollectionItem__edit"
-                title="Edit Color"
-                aria-label="Edit Color"
-                onClick={e => this.editColor(e, { id: colorId, name, color: colorValue })}
-              >
-                <IconPencil style={{ width: 14, height: 14 }} />
-              </div>
-              <div
-                className="CollectionItem__icon CollectionItem__copy"
-                title="Copy Color"
-                aria-label="Copy Color"
-                onClick={e => this.duplicateColor(e, { name, color: colorValue }, i)}
-              >
-                <IconCopy style={{ width: 14, height: 14 }} />
-              </div>
-              <div
-                className="CollectionItem__icon CollectionItem__remove"
-                title="Remove Color"
-                aria-label="Remove Color"
-                onClick={e => this.removeItem(e, { id: colorId, name, color: colorValue })}
-              >
-                <IconTrash style={{ width: 14, height: 14 }} />
-              </div>
-            </CollectionItem>
-          ))}
-        </div>
+        <div className="Option__body Option__collection">{renderItems(values)}</div>
       </StyledOption>
     );
   }
