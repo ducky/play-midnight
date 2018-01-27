52cafbd4d58f1136ca5cb256dce2141c966ae85a
Chris Tieman <iam@christieman.com>
2018-01-27 02:37:08 -0500

    Cleaned up options Components, got rid of deep nested folders

--- src/options/Components.js
+++ src/options/Components.js
@@ -1,14 +1,15 @@
-import Core from './components/A - Core/Core';
-import FAB from './components/A - Core/FAB';
-import Logo from './components/A - Core/Logo';
-import AccentsOnly from './components/B - General/AccentsOnly';
-import Enabled from './components/B - General/Enabled';
-import Favicon from './components/C - Customize/Favicon';
-import Queue from './components/C - Customize/Queue';
-import Menus from './components/D - Menus/Menus';
-import Playlists from './components/E - Playlists/Playlists';
-import SoundSearch from './components/E - Playlists/SoundSearch';
+import AccentsOnly from './components/AccentsOnly';
+import Core from './components/Core';
+import Enabled from './components/Enabled';
+import FAB from './components/FAB';
+import Favicon from './components/Favicon';
+import Logo from './components/Logo';
+import Menus from './components/Menus';
+import Playlists from './components/Playlists';
+import Queue from './components/Queue';
+import SoundSearch from './components/SoundSearch';
 
+// Categorized for My Sanity
 const CORE = [Core, FAB, Logo];
 const GENERAL = [AccentsOnly, Enabled];
 const CUSTOMIZE = [Favicon, Queue];
